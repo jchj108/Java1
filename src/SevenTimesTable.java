d497392d4ddfea4e9be1c94688d25acc23ecbf75
jchj108 <jchj108@naver.com>
2021-01-06 18:20:30 +0900

    init commit(while문, for문)

---
+++ src/SevenTimesTable.java
@@ -0,0 +1,19 @@
+public class SevenTimesTable {
+	public static void main(String[] args) {
+		
+		//6단 while 문
+		
+		int i = 1;
+		while(i < 10) {
+			System.out.println(6 * i);
+			i = i + 1;
+		}
+		
+		
+		//7단 for 문
+
+		for(int j = 1; j < 10; j++) {
+			System.out.println(7 * j);
+		}
+	}
+}
