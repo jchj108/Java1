64befd5815abef611b86cce8b39d5664b36d08e2
jchj108 <jchj108@naver.com>
2021-01-06 17:31:05 +0900

    init commit (variable)

---
+++ src/fiveTimesTable.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class fiveTimesTable {
+    public static void main(String[] args) {
+    	
+    	System.out.println("4단");
+    	int result = 4 * 1;
+    	System.out.println(result);
+    	result = 4 * 2;
+    	System.out.println(result);
+    	result = 4 * 3;
+    	System.out.println(result);
+    	result = 4 * 4;
+    	System.out.println(result);
+    	result = 4 * 5;
+    	System.out.println(result);
+    	result = 4 * 6;
+    	System.out.println(result);
+    	result = 4 * 7;
+    	System.out.println(result);
+    	result = 4 * 8;
+    	System.out.println(result);
+    	result = 4 * 9;
+    	System.out.println(result);
+    	
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("number : " + number);
+
+           
+        // 구구단 출력하는 예제 구현        
+    }
+}
